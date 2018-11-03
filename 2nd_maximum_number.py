7b7a7085ed19e5ecb71b07fda6fe5a6729efe5b8
wanghm <ohkai@nttdata-bizsys.co.jp>
2018-11-03 10:51:24 +0000

    find 2nd maximum number

---
+++ 2nd_maximum_number.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    l_arr = list(arr)
+    max_score = max(l_arr)
+    
+    while max(l_arr) == max_score:
+        l_arr.remove(max_score)
+    
+    print(max(l_arr))
+
