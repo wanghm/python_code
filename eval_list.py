4144e572531cce139fe81e1ce5eebf0b8fb82df1
wanghm <ohkai@nttdata-bizsys.co.jp>
2018-11-03 11:22:15 +0000

    List (eval) Hackerrank

---
+++ eval_list.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    list = []
+    for i in range(n):
+        a = input().split()
+        if len(a) == 3:
+            eval("list." + a[0] + "(" + a[1] + "," + a[2] + ")")
+        elif len(a) == 2:
+            eval("list." + a[0] + "(" + a[1] + ")")
+        elif a[0] == "print":
+            print(list)
+        else:
+            eval("list." + a[0] + "()")
+            
