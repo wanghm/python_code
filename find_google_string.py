afa21dced071a588f0f7f3aaa2e22ddc8e8295d6
Huimin Wang <433952+wanghm@users.noreply.github.com>
2018-12-15 10:41:56 +0900

    Update find_google_string.py

--- find_google_string.py
+++ find_google_string.py
@@ -3,6 +3,7 @@ g = G
 o = O = 0 = () = [] = <>
 l = L = I
 e = E = 3
+https://regexper.com/#%28G%7Cg%29%28O%7Co%7C0%7C%3C%3E%7C%5C%28%5C%29%7C%5C%5B%5C%5D%29%7B2%7D%28G%7Cg%29%28L%7Cl%7CI%29%28E%7Ce%7C3%29
 """
 
 #!/bin/python3
